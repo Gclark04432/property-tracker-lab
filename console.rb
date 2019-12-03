f4b1ec853647e6348a1c9728f7bb1dc9c6300325
Gary Clark <gclark04432@live.co.uk>
2019-12-03 15:19:53 +0000

    create-two-instances-of-property-class

--- console.rb
+++ console.rb
@@ -1,1 +1,16 @@
-# house1 = Property.new()
+require ('pry')
+require_relative ('models/properties')
+
+house1 = Property.new({
+  'value' => 100_000,
+  'bedrooms' => 1,
+  'buy_or_let' => "buy",
+  'year_built' => 1990
+  })
+
+house2 = Property.new({
+  'value' => 1000,
+  'bedrooms' => 2,
+  'buy_or_let' => "let",
+  'year_built' => 1995
+  })
